e50c19ab3094546a8d34ef4a74763888d357ea1b
Sunil Chaudhary <1234huts@gmail.com>
2023-05-07 10:42:46 +0530

    updated css and js

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,49 +1,69 @@
 "use strict";
 
-let date = new Date();
-let year = date.getFullYear();
+function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+var date = new Date();
+var year = date.getFullYear();
 
 // set nav click on hover for nav dropdown
-const nav = document.querySelector("nav");
-const dropdown = Array.from(nav.querySelectorAll(".nav-item.dropdown [data-toggle]"));
+var nav = document.querySelector("nav");
+var dropdown = Array.from(nav.querySelectorAll(".nav-item.dropdown [data-toggle]"));
 if (window.innerWidth > 1000) {
-  dropdown.forEach(el => {
-    el.addEventListener("mouseover", () => {
+  // Open dropdown on mouseover
+  dropdown.forEach(function (el) {
+    el.addEventListener("mouseover", function () {
       el.click();
     });
   });
-  document.addEventListener("mouseout", event => {
+
+  // Close dropdowns on mouseout
+  document.addEventListener("mouseout", function (event) {
     if (!dropdown.includes(event.target) && !event.target.classList.contains("dropdown-menu") && !event.target.classList.contains("dropdown-item") && !event.target.closest(".dropdown-submenu")) {
       document.body.click();
     }
   });
 }
 if (window.innerWidth < 1000) {
-  dropdown.forEach(el => {
-    el.addEventListener("click", () => {
+  // Dropdown click event listener
+  dropdown.forEach(function (el) {
+    el.addEventListener("click", function () {
       if (window.innerWidth < 992) {
-        const submenu = el.nextElementSibling;
+        var submenu = el.nextElementSibling;
+        console.log(submenu);
         if (submenu.classList.contains("show")) {
-          setTimeout(() => {
+          // Close the submenu
+          setTimeout(function () {
             submenu.classList.remove("show");
             submenu.parentElement.classList.remove("show");
           }, 100);
         } else {
-          const activeSubmenus = nav.querySelectorAll(".dropdown-submenu.show");
-          activeSubmenus.forEach(subsubmenu => {
+          // Open the submenu
+          var activeSubmenus = nav.querySelectorAll(".dropdown-submenu.show");
+          activeSubmenus.forEach(function (subsubmenu) {
             subsubmenu.classList.remove("show");
           });
           submenu.classList.toggle("show");
-        }
-        let submenus = submenu.querySelectorAll(".dropdown-item.dropdown-toggle");
-        submenus.forEach(m => {
-          m.addEventListener("click", () => {
-            const element = m.nextElementSibling;
-            const displayStyle = window.getComputedStyle(element).display;
-            if (displayStyle === "block") {
-              element.style.display = "none";
-            } else {
-              element.style.display = "block";
+
+          // Add activeDropdown class to the parent element
+          var parentElement = el.parentElement;
+          var otherDropdownParents = Array.from(dropdown).map(function (dropdownEl) {
+            return dropdownEl.parentElement;
+          });
+          otherDropdownParents.forEach(function (dropdownParent) {
+            dropdownParent.classList.remove("activeDropdown");
+          });
+          parentElement.classList.add("activeDropdown");
+
+          // Close other dropdowns
+          var otherDropdowns = Array.from(dropdown).filter(function (dropdownEl) {
+            return dropdownEl !== el;
+          });
+          otherDropdowns.forEach(function (dropdownEl) {
+            var otherSubmenu = dropdownEl.nextElementSibling;
+            if (otherSubmenu.classList.contains("show")) {
+              otherSubmenu.classList.remove("show");
+              otherSubmenu.parentElement.classList.remove("show");
             }
           });
         }
@@ -52,10 +72,11 @@ if (window.innerWidth < 1000) {
       }
     });
   });
-  document.addEventListener("click", event => {
+  document.addEventListener("click", function (event) {
     if (!dropdown.includes(event.target) && !event.target.classList.contains("dropdown-menu") && !event.target.classList.contains("dropdown-item") && !event.target.closest(".dropdown-submenu")) {
-      const activeSubmenus = nav.querySelectorAll(".dropdown-submenu.show");
-      activeSubmenus.forEach(submenu => {
+      // Close all active submenus
+      var activeSubmenus = nav.querySelectorAll(".dropdown-submenu.show");
+      activeSubmenus.forEach(function (submenu) {
         submenu.classList.remove("show");
       });
 
@@ -95,11 +116,11 @@ if (window.innerWidth < 1000) {
 }
 
 // nav mobile toggle btn code
-const navbarToggler = document.querySelector(".navbar-toggler");
-const navbarCollapse = document.querySelector(".navbar-collapse");
-navbarToggler.addEventListener("click", () => {
-  let isShown = navbarCollapse.classList.contains("show");
-  setTimeout(() => {
+var navbarToggler = document.querySelector(".navbar-toggler");
+var navbarCollapse = document.querySelector(".navbar-collapse");
+navbarToggler.addEventListener("click", function () {
+  var isShown = navbarCollapse.classList.contains("show");
+  setTimeout(function () {
     if (isShown) {
       navbarCollapse.style.maxHeight = "0";
     } else {
@@ -109,7 +130,7 @@ navbarToggler.addEventListener("click", () => {
   }, 10);
 });
 document.addEventListener("DOMContentLoaded", function () {
-  let formConfig = {
+  var formConfig = {
     // class of the parent element where the error/success class is added
     classTo: "form-group",
     errorClass: "has-danger",
@@ -134,23 +155,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // counter animation
   // Get all the counter elements
-  const counterElements = document.querySelectorAll(".countings__stat");
+  var counterElements = document.querySelectorAll(".countings__stat");
 
   // counter animation
-  let counterOptions = {
+  var counterOptions = {
     rootMargin: "-50px",
     threshold: 0.5
   };
-  let counterCallback = (entries, observer) => {
-    entries.forEach(entry => {
+  var counterCallback = function counterCallback(entries, observer) {
+    entries.forEach(function (entry) {
       if (entry.isIntersecting) {
-        let counterElement = entry.target;
-        const duration = parseInt(counterElement.getAttribute("data-count-duration"));
-        const countElement = counterElement.querySelector(".statCount");
-        const updateCounter = () => {
-          const target = +counterElement.getAttribute("data-count");
-          const count = +countElement.innerText;
-          const increment = target / duration;
+        var counterElement = entry.target;
+        var duration = parseInt(counterElement.getAttribute("data-count-duration"));
+        var countElement = counterElement.querySelector(".statCount");
+        var updateCounter = function updateCounter() {
+          var target = +counterElement.getAttribute("data-count");
+          var count = +countElement.innerText;
+          var increment = target / duration;
           if (count < target) {
             countElement.innerText = "".concat(Math.ceil(count + increment));
             setTimeout(updateCounter, 1);
@@ -161,54 +182,63 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   };
-  let counterObserver = new IntersectionObserver(counterCallback, counterOptions);
+  var counterObserver = new IntersectionObserver(counterCallback, counterOptions);
 
   // Loop through each counter element and animate the count
-  counterElements.forEach(counterElement => {
+  counterElements.forEach(function (counterElement) {
     counterObserver.observe(counterElement);
   });
 
   // mutation observer for accordions
   // Select the node that will be observed for mutations
-  const targetNode = document;
+  var targetNode = document;
 
   // Options for the observer (which mutations to observe)
-  const config = {
+  var config = {
     attributes: true,
     childList: true,
     subtree: true
   };
 
   // Callback function to execute when mutations are observed
-  const callback = function (mutationsList, observer) {
+  var callback = function callback(mutationsList, observer) {
     // Loop through the mutations
-    for (const mutation of mutationsList) {
-      // Check if the mutation is a change in attribute
-      if (mutation.type === "attributes" && mutation.attributeName === "aria-expanded") {
-        // Select the closest parent with the class "c-accordion"
-        const accordion = mutation.target.closest(".c-accordion");
-        if (!accordion) {
-          continue;
-        }
-        // Add or remove the class "-active" based on the attribute value
-        if (mutation.target.getAttribute("aria-expanded") === "true") {
-          accordion.classList.add("-active");
-        } else {
-          accordion.classList.remove("-active");
+    var _iterator = _createForOfIteratorHelper(mutationsList),
+      _step;
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        var mutation = _step.value;
+        // Check if the mutation is a change in attribute
+        if (mutation.type === "attributes" && mutation.attributeName === "aria-expanded") {
+          // Select the closest parent with the class "c-accordion"
+          var accordion = mutation.target.closest(".c-accordion");
+          if (!accordion) {
+            continue;
+          }
+          // Add or remove the class "-active" based on the attribute value
+          if (mutation.target.getAttribute("aria-expanded") === "true") {
+            accordion.classList.add("-active");
+          } else {
+            accordion.classList.remove("-active");
+          }
         }
       }
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
     }
   };
 
   // Create an observer instance linked to the callback function
-  const observer = new MutationObserver(callback);
+  var observer = new MutationObserver(callback);
 
   // Start observing the target node for configured mutations
   observer.observe(targetNode, config);
   var accordions = document.querySelectorAll(".c-accordion__btn");
-  accordions.forEach(btn => {
-    btn.addEventListener("click", () => {
-      accordions.forEach(cbtn => {
+  accordions.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+      accordions.forEach(function (cbtn) {
         if (cbtn != btn) {
           cbtn.classList.add("collapsed");
           cbtn.setAttribute("aria-expanded", "false");
@@ -219,11 +249,11 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // event for scaitabs
-  let scaiTabs = document.querySelector(".scaiTabs");
+  var scaiTabs = document.querySelector(".scaiTabs");
   if (scaiTabs && window.innerWidth >= 1020) {
-    let tabButtons = Array.from(scaiTabs.querySelectorAll(".scaiTabs .d-md-flex .nav .nav-link"));
-    tabButtons.map(btn => {
-      btn.addEventListener("click", () => {
+    var tabButtons = Array.from(scaiTabs.querySelectorAll(".scaiTabs .d-md-flex .nav .nav-link"));
+    tabButtons.map(function (btn) {
+      btn.addEventListener("click", function () {
         window.scrollTo({
           top: scaiTabs.offsetTop,
           left: 0,
@@ -234,8 +264,8 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 var videoBtns = document.querySelectorAll("button[data-video]");
-videoBtns.forEach(btn => {
-  btn.addEventListener("click", () => {
+videoBtns.forEach(function (btn) {
+  btn.addEventListener("click", function () {
     var videoEle = document.querySelector("".concat(btn.dataset.target, " video"));
     videoEle.setAttribute("src", btn.dataset.video);
   });
